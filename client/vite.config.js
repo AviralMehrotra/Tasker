5dc489c6d8e65a4da0b4a3aec54ff0f67a2d6a7e
Aviral Mehrotra <73425338+AviralMehrotra@users.noreply.github.com>
2025-07-19 09:10:41 +0530

    Vite config fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
 
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-    },
+    //only for development
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:8000",
+    //     changeOrigin: true,
+    //   },
+    // },
   },
 });
